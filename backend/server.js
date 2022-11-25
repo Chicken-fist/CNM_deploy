9f82cfa51fc54206c3df28b92784ad26ba8526ad
ADMIN <ndang1336@gmail.com>
2022-11-25 14:38:27 +0700

    len

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,6 @@ const server = app.listen(PORT || 5000, () => {
   console.log(`http://localhost:${PORT}`);
 });
 
-const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
